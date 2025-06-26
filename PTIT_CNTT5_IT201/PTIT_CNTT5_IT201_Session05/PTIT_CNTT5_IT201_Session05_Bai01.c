f8e864fe3a79983f82542d942d15d42dc0786d5f
Haidang2101 <haidanghanoi2003@gmail.com>
2025-06-26 21:36:48 +0700

    Add files via upload

---
+++ PTIT_CNTT5_IT201/PTIT_CNTT5_IT201_Session05/PTIT_CNTT5_IT201_Session05_Bai01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void inSo(int n) {
+    if (n == 0) return;
+    inSo(n - 1);
+    printf("%d ", n);
+}
+
+int main() {
+    int n;
+
+    printf("Nhap mot so nguyen duong: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Vui long nhap so nguyen duong!\n");
+    } else {
+        printf("Cac so tu 1 den %d la:\n", n);
+        inSo(n);
+        printf("\n");
+    }
+
+    return 0;
+}
+
